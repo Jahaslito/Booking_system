5c6d8b58e633a8f42b81e37400c0da26232c5809
Georgina7 <nyokabigeorgina007@gmail.com>
2021-06-12 14:07:29 +0300

    Tailwind Setup

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('test');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

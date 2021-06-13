715e83c96ec1ec9eb643583bc5a9e3353a9a848d
Georgina7 <nyokabigeorgina007@gmail.com>
2021-06-13 21:28:56 +0300

    Additional Dashboard Changes - Incomplete

--- resources/views/adminviews/routes.blade.php
+++ resources/views/adminviews/routes.blade.php
@@ -9,12 +9,187 @@
 <body>
 	<section>
 		<article>
+			<div id="overlay" class=" bg-black bg-opacity-50 absolute inset-0 hidden justify-center items-center">
+				<div class="bg-gray-200">
+					<div class="flex justify-between items-center">
+						<h4 class="text-lg font-semibold">My Profile</h4>
+						<svg id="close-profile-modal" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</div>
+					<div>
+						<label>Name</label>	
+						<input type="text" name="name" value="Jane Mbiru" readonly>
+						<br>
+						<label>Name</label>	
+						<input type="text" name="name" value="Jane Mbiru" readonly>
+						<br>
+						<label>Name</label>	
+						<input type="text" name="name" value="Jane Mbiru" readonly>
+						<br>						
+					</div>
+					<div class="flex justify-center items-center">
+						<button>Edit</button>
+					</div>
+				</div>        
+    		</div>
+    		<div id="edit_modal" class=" bg-black bg-opacity-50 absolute inset-0 hidden justify-center items-center">
+				<div class="bg-gray-50 p-3 rounded-lg">
+					<div class="flex justify-between items-center">
+						<h4 class="text-lg font-semibold">Edit Route</h4>
+						<svg onclick="closeEditModal()" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</div>
+					<div class="grid grid-cols-6 gap-6 p-4">
+						<div class="col-span-6 sm:col-span-3">
+			                <label for="source" class="block text-sm font-medium text-gray-700">Source</label>
+			                <input type="text" name="source" id="source" value="Nairobi" readonly class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md p-2">
+			            </div>
+			            <div class="col-span-6 sm:col-span-3">
+			                <label for="destination" class="block text-sm font-medium text-gray-700">Destination</label>
+			                <input type="text" name="destination" id="destination" value="Kisumu" readonly class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md p-2">
+			            </div>
+			            <div class="col-span-6 sm:col-span-3">
+			                <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
+			                <input type="number" name="price" id="price" value="2500" readonly class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md p-2">
+			            </div>					
+					</div>
+					<div class="flex justify-center items-center mb-3">
+						<button class="bg-gray-900 px-3 py-1 text-gray-50 rounded">Edit</button>
+					</div>
+				</div>        
+    		</div>
+    		<div id="add_modal" class=" bg-black bg-opacity-50 absolute inset-0 hidden justify-center items-center">
+				<div class="bg-gray-50 p-3 rounded-lg">
+					<div class="flex justify-between items-center">
+						<h4 class="text-lg font-semibold">Add Route</h4>
+						<svg onclick="closeAddModal()" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</div>
+					<div class="grid grid-cols-6 gap-6 p-4">
+						<div class="col-span-6 sm:col-span-3">
+			                <label for="source" class="block text-sm font-medium text-gray-700">Source</label>
+			                <input type="text" name="source" id="source" placeholder="Nairobi" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md p-2">
+			            </div>
+			            <div class="col-span-6 sm:col-span-3">
+			                <label for="destination" class="block text-sm font-medium text-gray-700">Destination</label>
+			                <input type="text" name="destination" id="destination" placeholder="Kisumu" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md p-2">
+			            </div>
+			            <div class="col-span-6 sm:col-span-3">
+			                <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
+			                <input type="number" name="price" id="price" placeholder="2500" min="0" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md p-2">
+			            </div>									
+					</div>
+					<div class="flex justify-center items-center">
+						<button class="bg-gray-900 px-3 py-1 text-gray-50 rounded">Add</button>
+					</div>
+				</div>        
+    		</div>
 			<div class="box-content p-2 m-1 bg-red-900 rounded-lg ">
                 <center class="text-xl cursor-default ">Routes</center>
             </div>
-			<p>This is the routes page</p>
+			<div class="m-2">
+				<table class="min-w-full divide-y divide-gray-200 box-border">
+			          <thead class="bg-gray-50">
+			            <tr>
+			              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+			                Number
+			              </th>
+			              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+			                Source
+			              </th>
+			              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+			                Destination
+			              </th>
+			              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+			                Price
+			              </th>
+			              <th scope="col" class="relative px-6 py-3">
+			                <span class="sr-only">Edit</span>
+			              </th>
+			            </tr>
+			          </thead>
+			          <tbody class="bg-white divide-y divide-gray-200">
+			            <tr>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="flex items-center">
+			                  <div class="ml-4">
+			                    <div class="text-sm font-medium text-gray-900">
+			                      1
+			                    </div>
+			                  </div>
+			                </div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="text-sm text-gray-900">Nairobi</div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="text-sm text-gray-900">Kisumu</div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+			                2500
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+			                <button onclick="editModal()" class="text-indigo-600 hover:text-indigo-900">Edit</button>
+			              </td>
+			            </tr>
+			            <tr>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="flex items-center">
+			                  <div class="ml-4">
+			                    <div class="text-sm font-medium text-gray-900">
+			                      2
+			                    </div>
+			                  </div>
+			                </div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="text-sm text-gray-900">Nairobi</div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="text-sm text-gray-900">Kisumu</div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+			                2500
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+			                <button onclick="editModal()" class="text-indigo-600 hover:text-indigo-900">Edit</button>
+			              </td>
+			            </tr>
+			            <tr>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="flex items-center">
+			                  <div class="ml-4">
+			                    <div class="text-sm font-medium text-gray-900">
+			                      3
+			                    </div>
+			                  </div>
+			                </div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="text-sm text-gray-900">Nairobi</div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap">
+			                <div class="text-sm text-gray-900">Kisumu</div>
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+			                2500
+			              </td>
+			              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+			                <button onclick="editModal()" class="text-indigo-600 hover:text-indigo-900">Edit</button>
+			              </td>
+			            </tr>
+
+
+			            <!-- More people... -->
+			          </tbody>
+			        </table>
+			</div>	
+			<button onclick="addModal()" class="absolute right-0 bottom-0 p-2 px-4 m-3 fixed bg-red-900 rounded-lg">Add</button>	
 		</article>
 	</section>
-
+<script src="{{url('/js/components.js')}}"></script>
 </body>
 </html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,24 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
 </head>
 <body class="bg-gray-900">
-    <!-- div class="box-border p-9 border-4 pt-0"> -->
-        <div class="h-14 mt-2">
-            
-        
-        <div class="relative h-12 flex">
-            <div class="p-3 text-gray-50 cursor-pointer ml-6">LOGO</div>
-            <div>
-                <input type="text" name="Search" placeholder="Search..." class="absolute inset-x-2/4 border-1 m-1 mt-1.5 ml-4 p-1 focus:outline-none focus:ring focus:border-blue-300 rounded-full py-2 px-5">
+    
+        <div class="h-14 mt-2">       
+            <div class="relative h-12 flex">
+                <div class="p-3 text-gray-50 cursor-pointer ml-6">LOGO</div>
+                <div>
+                    <input type="text" name="Search" placeholder="Search..." class="absolute inset-x-2/4 border-1 m-1 mt-1.5 ml-4 p-1 focus:outline-none focus:ring focus:border-blue-300 rounded-full py-2 px-5">
+                </div>
+                <div class="absolute right-3">
+                    <button class="box-content p-1 px-3 bg-red-900 m-1 mt-2 rounded-lg" id="profile-btn">Profile</button>
+                    <button class="box-content p-1 px-3 bg-red-900 m-1 mt-2 rounded-lg">Logout</button>                
+                </div>
             </div>
-            <div class="absolute right-3">
-                <button class="box-content p-1 px-3 bg-red-900 m-1 mt-2 rounded-lg">Profile</button>
-                <button class="box-content p-1 px-3 bg-red-900 m-1 mt-2 rounded-lg">Logout</button>                
-            </div>
-        </div>
         </div>
         <div class="box-border flex max-h-full ">
             <div class="w-1/5 p-3">
@@ -32,27 +28,51 @@
                     <div class="rounded-full h-24 w-24 flex items-center justify-center border-4 justify-self-center overflow-hidden"><img src="{{url('/images/avatar.png')}}"></div>
                 </center>  
                 <br>        
-                <div class="m-2 box-content p-2 my-1 "><i class="fa-regular fa-location"></i><a href="{{url('/adminviews/landing')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">DASHBOARD</a></div>
+                <div class="m-2 box-content p-2 my-1 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                    <a href="{{url('/adminviews/landing')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">DASHBOARD</a>
 
-                <div class="m-2 box-content p-2 my-1 "><i class="fa-regular fa-location"></i><a href="{{url('/adminviews/routes')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">ROUTES</a></div>
+                </div>
 
-                <div class="m-2 box-content p-2 my-1 "><i class="fa-regular fa-location"></i><a href="{{url('/adminviews/buses')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">BUSES</a></div>
+                <div class="m-2 box-content p-2 my-1 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                    <a href="{{url('/adminviews/routes')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">ROUTES</a>
+                </div>
 
-                <div class="m-2 box-content p-2 my-1 "><i class="fa-regular fa-location"></i><a href="{{url('/adminviews/trips')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">TRIPS</a></div>
+                <div class="m-2 box-content p-2 my-1 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+                    </svg>
+                    <a href="{{url('/adminviews/buses')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">BUSES</a>
+                </div>
 
-                <div class="m-2 box-content p-2 my-1 "><i class="fa-regular fa-location"></i><a href="{{url('/adminviews/drivers')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">DRIVERS</a></div>
+                <div class="m-2 box-content p-2 my-1 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <a href="{{url('/adminviews/trips')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">TRIPS</a>
+                </div>
+
+                <div class="m-2 box-content p-2 my-1 flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    <a href="{{url('/adminviews/drivers')}}" target="frame" class="text-xl text-gray-50 hover:text-red-900 cursor-pointer">DRIVERS</a>
+                </div>
             </div>
-            <div class="relative w-4/5 mr-3 bg-gray-50 rounded-lg">                
-                <main>
-                  <div class="iframe">
-                    <iframe src="{{url('/adminviews/landing')}}" name="frame" class="box-border rounded-lg w-full h-full relative"></iframe>
-                  </div>
-                </main>
-                <button class="absolute right-0 bottom-0 p-2 px-4 m-3 fixed bg-red-900 rounded-lg">Add</button>
+            <div class="relative w-4/5 mr-3 bg-gray-50 rounded-lg">
+                <iframe id="dash-frame" src="{{url('/adminviews/landing')}}" name="frame" class="box-border rounded-lg w-full h-full relative">                    
+                </iframe>                
             </div>
-        </div>        
-    <!-- </div> -->
-    
+        </div>
+
+<script src="{{url('/js/dashboard.js')}}"></script>            
 
 </body>
 </html>
